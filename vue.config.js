6f99de919563fd99880232ac499d1d4dbc0476e3
liuzepo <873717352@qq.com>
2019-03-24 21:12:38 +0800

    解开分割代码

--- vue.config.js
+++ vue.config.js
@@ -59,9 +59,9 @@ module.exports = {
     // 压缩代码
     config.optimization.minimize(true);
     // 分割代码
-    // config.optimization.splitChunks({
-    //   chunks: 'all'
-    // })
+    config.optimization.splitChunks({
+      chunks: 'all'
+    })
     // 用cdn方式引入
     config.externals({
       'vue': 'Vue',
