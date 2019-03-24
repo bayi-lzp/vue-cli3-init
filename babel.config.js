98caf66361b801e29ce9c81db6cc58a4edec9fbd
liuzepo <873717352@qq.com>
2019-03-24 14:57:08 +0800

    解决去除log的bug

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,18 @@
 // 去除生产环境的consoles
-const plugins = [];
-if(['production', 'prod'].includes(process.env.NODE_ENV)) {
-  plugins.push("transform-remove-console")
+let plugins = {};
+if (['production', 'prod'].includes(process.env.NODE_ENV)) {
+    plugins = "transform-remove-console"
 }
 module.exports = {
-  presets: [["@vue/app",{"useBuiltIns": "entry"}]],
-  plugins: [
-      // plugins,
-    [
-      "component",
-      {
-        "libraryName": "mint-ui",
-        "style": true
-      }
+    presets: [["@vue/app", {"useBuiltIns": "entry"}]],
+    plugins: [
+        plugins,
+        [
+            "component",
+            {
+                "libraryName": "mint-ui",
+                "style": true
+            }
+        ]
     ]
-  ]
 };
