140e448b27c5f481010a6a86fbf68cd56668a617
bayi-lzp <32999059+bayi-lzp@users.noreply.github.com>
2019-05-22 21:21:14 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,17 +2,17 @@
 const modeUrlObj = {
   // 生产环境
   'production': {
-    baseURL: 'http://172.17.71.40:9091/pro/',
+    baseURL: 'http://xxxx:9091/pro/',
     authBaseURL: ''
   },
   // 开发环境
   'development': {
-    baseURL: 'http://172.17.71.40:9091/dev/',
+    baseURL: 'http://xxxx:9091/dev/',
     authBaseURL: ''
   },
   // 测试环境
   'test': {
-    baseURL: 'http://172.17.71.40:9091/test/',
+    baseURL: 'http://xxxxx:9091/test/',
     authBaseURL: ''
   }
 }
